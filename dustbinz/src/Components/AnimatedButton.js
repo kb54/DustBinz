5bdcf10250166a0440b56e5cf8e9829ac709996d
ishanExtreme <ishan2198@hotmail.com>
2021-03-03 17:40:50 +0530

    button responsive

--- dustbinz/src/Components/AnimatedButton.js
+++ dustbinz/src/Components/AnimatedButton.js
@@ -4,8 +4,9 @@ import Icon from '@mdi/react';
 
 function AnimatedButton({text, path}) {
     return (
+    <>
         <motion.div
-            className="anim-button btn btn-success"
+            className="anim-button btn btn-success d-none d-lg-block"
             whileHover={{scale:1.1}}
             whileTap={{scale:0.9}}
         >
@@ -15,6 +16,19 @@ function AnimatedButton({text, path}) {
             size={3}
             />
         </motion.div>
+
+        <motion.div
+        className="anim-button btn btn-success d-sm-block d-lg-none"
+        whileHover={{scale:1.1}}
+        whileTap={{scale:0.9}}
+        >
+        <h style={{fontSize:25}}>{text}</h>
+        <Icon
+        path={path}
+        size={3}
+        />
+        </motion.div>
+    </>
     );
 }
 
